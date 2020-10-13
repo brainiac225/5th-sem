272ad85d509a13d3593b8f0a8b688eb4c0a7e66e
Ahmed Ali <53947985+brainiac225@users.noreply.github.com>
2020-10-13 15:05:02 +0530

    Create fact.sh

---
+++ USP LAB/fact.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+read -p "Enter a number: " num
+
+fact=1
+
+while [ $num -gt 1 ]
+do
+  fact=$((fact * num))  #fact = fact * num
+  num=$((num - 1))      #num = num - 1
+done
+
+echo "The factorial is:" $fact
